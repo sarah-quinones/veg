0229fd566a3bf5410bd02568c4ec8180c63f0736
sarah <>
2021-04-12 13:21:47 +0200

    copy_fn fix

--- include/veg/functional/copy_fn.hpp
+++ include/veg/functional/copy_fn.hpp
@@ -12,6 +12,7 @@ namespace fn {
 template <typename Fn>
 struct CopyFn {
 	VEG_CHECK_CONCEPT(copy_constructible<Fn>);
+	VEG_CHECK_CONCEPT(same<Fn, meta::decay_t<Fn>>);
 
 	Fn fn;
 	VEG_TEMPLATE(
@@ -28,21 +29,22 @@ struct CopyFn {
 	}
 };
 namespace nb {
-struct copy_fn_fwd {
+struct copy_fn {
 	VEG_TEMPLATE(
 			typename Fn,
 			requires(
-					VEG_CONCEPT(move_constructible<Fn>) &&
-					VEG_CONCEPT(copy_constructible<Fn>)),
+					VEG_CONCEPT(constructible<meta::decay_t<Fn>, Fn&&>) &&
+					VEG_CONCEPT(copy_constructible<meta::decay_t<Fn>>)),
 			HEDLEY_ALWAYS_INLINE constexpr auto
 			operator(),
 			(fn, Fn&&))
-	const noexcept(VEG_CONCEPT(nothrow_move_constructible<Fn>))->CopyFn<Fn> {
-		return {VEG_FWD(fn)};
+	const noexcept(VEG_CONCEPT(nothrow_move_constructible<Fn>))
+			->CopyFn<meta::decay_t<Fn>> {
+		return {meta::decay_t<Fn>(VEG_FWD(fn))};
 	}
 };
 } // namespace nb
-VEG_NIEBLOID(copy_fn_fwd);
+VEG_NIEBLOID(copy_fn);
 } // namespace fn
 } // namespace VEG_ABI
 } // namespace veg
