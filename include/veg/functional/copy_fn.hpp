1f28b219a9855c6024374e3baef65f493d93bd1f
sarah <>
2021-04-12 13:09:29 +0200

    copy fn

---
+++ include/veg/functional/copy_fn.hpp
@@ -0,0 +1,33 @@
+#ifndef VEG_COPY_FN_HPP_FY5B8PSTS
+#define VEG_COPY_FN_HPP_FY5B8PSTS
+
+#include "veg/type_traits/constructible.hpp"
+#include "veg/type_traits/invocable.hpp"
+#include "veg/functional/utils.hpp"
+#include "veg/internal/prologue.hpp"
+
+namespace veg {
+inline namespace VEG_ABI {
+namespace fn {
+template <typename Fn>
+struct CopyFn {
+	VEG_CHECK_CONCEPT(copy_constructible<Fn>);
+
+	Fn fn;
+	VEG_TEMPLATE(
+			typename... Args,
+			requires(VEG_CONCEPT(invocable<Fn, Args&&...>)),
+			HEDLEY_ALWAYS_INLINE constexpr auto
+			operator(),
+			(... args, Args&&))
+	const noexcept(VEG_CONCEPT(nothrow_invocable<Fn, Args&&...>))
+			->meta::invoke_result_t<Fn, Args&&...> {
+		return (clone)(fn)(VEG_FWD(args)...);
+	}
+};
+} // namespace fn
+} // namespace VEG_ABI
+} // namespace veg
+
+#include "veg/internal/epilogue.hpp"
+#endif /* end of include guard VEG_COPY_FN_HPP_FY5B8PSTS */
